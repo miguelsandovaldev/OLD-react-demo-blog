1933353b9f26dad7ed08269a9dc1609578f0fe0c
miguelsandovaldev <miguelsandovaldev@gmail.com>
2020-03-10 23:28:20 -0400

    Completed section 14

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
+import postsReducer from "./postsReducer";
 
 export default combineReducers({
-  replaceMe: () => "this is dummy string to prevent errors from redux"
+  posts: postsReducer
 });
