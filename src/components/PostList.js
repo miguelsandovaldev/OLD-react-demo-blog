4b5e368ed03119a591c68cca6650fc09e56ada0e
msandovalwup <msandoval@wheelsup.com>
2020-03-12 09:45:40 -0400

    Added User display with memoization via lodash

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { fetchPosts } from "../actions";
+import UserHeader from "./UserHeader";
 
 class PostList extends React.Component {
   componentDidMount() {
@@ -10,15 +11,16 @@ class PostList extends React.Component {
   renderList() {
     return this.props.posts.map(post => {
       return (
-          <div className="item" key={post.id}>
-            <i className="large middle aligned icon user" />
-            <div className="content">
-              <div className="description">
-                <h2>{post.title}</h2>
-                <p>{post.body}</p>
-              </div>
+        <div className="item" key={post.id}>
+          <i className="large middle aligned icon user" />
+          <div className="content">
+            <div className="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
             </div>
+            <UserHeader userId={post.userId} />
           </div>
+        </div>
       );
     });
   }
@@ -30,6 +32,6 @@ class PostList extends React.Component {
 
 const mapStateToProps = state => {
   return { posts: state.posts };
-}
+};
 
 export default connect(mapStateToProps, { fetchPosts })(PostList);
